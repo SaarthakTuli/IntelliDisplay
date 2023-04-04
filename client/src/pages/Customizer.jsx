4c096696d6d01ec83cd01df766bb038a7dadbdf9
SaarthakTuli <saarthaktuli@gmail.com>
2023-04-05 01:40:30 +0530

    colorpicker

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -20,6 +20,29 @@ import {
 const Customizer = () => {
   const snap = useSnapshot(state);
 
+  const [file, setFile] = useState("");
+  const [prompt, setPromtp] = useState("");
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+      case "filepicker":
+        return <FilePicker />;
+      case "aipicker":
+        return <AIPicker />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -32,9 +55,15 @@ const Customizer = () => {
           >
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
-                {EditorTabs.map((tab) => {
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />;
-                })}
+                {EditorTabs.map((tab) => (
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
+                  />
+                ))}
+
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
@@ -55,15 +84,15 @@ const Customizer = () => {
             className="filtertabs-container"
             {...slideAnimation("up")}
           >
-            {FilterTabs.map((tab) => {
+            {FilterTabs.map((tab) => (
               <Tab
                 key={tab.name}
                 tab={tab}
                 isFilterTab
                 isActiveTab=""
                 handleClick={() => {}}
-              />;
-            })}
+              />
+            ))}
           </motion.div>
         </>
       )}
