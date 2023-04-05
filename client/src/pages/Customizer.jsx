393760578cc6ede0173a0f6b7fa5125c91320937
SaarthakTuli <saarthaktuli@gmail.com>
2023-04-05 21:54:30 +0530

    ai

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -59,12 +59,8 @@ const Customizer = () => {
 
       const response = await fetch("http://localhost:8080/api/v1/dalle", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt: "prompt",
-        }),
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ prompt }),
       });
 
       const data = await response.json();
--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -21,18 +21,19 @@ router.route("/").post(async (req, res) => {
     const { prompt } = req.body;
 
     const response = await openai.createImage({
-      prompt: "anime logo with no bg",
+      prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
     });
 
     const image = response.data.data[0].b64_json;
+    console.log("Image is: ", image);
 
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Something went wrong" });
+    res.status(500).json({ message: `Something went wrong, ERROR: ${error}` });
   }
 });
 
